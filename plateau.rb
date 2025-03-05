b68271ec0e9c3bd37fa5b1e6ae27d410e316a32d
Myles Amphlett <mylesamphlett@gmail.com>
2025-03-05 14:55:19 +0000

    create plateau grid with x & y coordinates as keys

--- plateau.rb
+++ plateau.rb
@@ -1,4 +1,27 @@
 class Plateau
-  def initialize()
+  attr_reader :x_coordinate, :y_coordinate, :grid
+
+  def initialize(x_coordinate, y_coordinate)
+    @x_coordinate = x_coordinate
+    @y_coordinate = y_coordinate
+    @grid = set_grid
+  end
+
+  private
+
+  def set_grid
+    grid = {}
+    x_grid = 0
+    while x_grid <= @x_coordinate
+      y_grid = 0
+      while y_grid <= @y_coordinate
+        # Create coordinate
+        grid[[x_grid, y_grid]] = false
+        y_grid += 1
+      end
+      x_grid += 1
+    end
+    grid
   end
 end
+
--- spec/plateau_spec.rb
+++ spec/plateau_spec.rb
@@ -1,5 +1,37 @@
 require_relative '../plateau'
 
 RSpec.describe 'Plateau' do
+  let(:plateau) { Plateau.new(5, 5) }
 
+  context 'top right coordinates' do
+    it 'has x coordinate' do
+      expect(plateau.x_coordinate).to eq(5)
+    end
+
+    it 'has y coordinate' do
+      expect(plateau.y_coordinate).to eq(5)
+    end
+
+    it 'cannot change x coordinate' do
+      expect{ plateau.x_coordinate = 6 }.to raise_error(NoMethodError)
+    end
+
+    it 'cannot change y coordinate' do
+      expect{ plateau.y_coordinate = 6 }.to raise_error(NoMethodError)
+    end
+  end
+
+  context 'grid' do
+    it 'exists' do
+      expect(plateau).to respond_to(:grid)
+    end
+
+    it 'is a hash' do
+      expect(plateau.grid.class).to eq(Hash)
+    end
+
+    it 'has the correct number of coordinates' do
+      expect(plateau.grid.keys.count).to eq(36)
+    end
+  end
 end
